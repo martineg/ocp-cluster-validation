ade7950206bbc291574ec22710f4199a6827429c
Martin Eggen <meggen@redhat.com>
2021-03-18 15:27:54 +0100

    No need to template config.yaml

--- cerberus/deploy-cerberus.sh
+++ cerberus/deploy-cerberus.sh
@@ -7,7 +7,7 @@ kubeconfig=../../kubeconfig
 oc apply -f cerberus-ns.yaml
 
 # create config maps
-oc create cm cerberus-config --from-file=config.yaml -n ${cerberus_ns} && rm config.yaml
+oc create cm cerberus-config --from-file=config.yaml -n ${cerberus_ns}
 oc create cm kube-config --from-file=config=${kubeconfig} -n ${cerberus_ns}
 
 # apply remaining deployment assets
